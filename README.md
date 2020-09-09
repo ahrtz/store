72251c4f1fcdfa0be8c90542a69f3ead29dea5d9
Inha Yoon <dlsgk147@naver.com>
2020-09-09 17:44:32 +0900

    ADD 09/09 개발 내용

--- README.md
+++ README.md
@@ -25,3 +25,14 @@ python mode_summarize.py
 
 ##### Excel -> 하나로 이어붙이기
 하나의 Excel파일로 하기로 했지만, for문으로 file들 읽어서 DB에 저장하면 어떨까 테스트
+
+## 09/09
+
+##### Vuetify 활용
+Vuetify에서 제공하는 file input, tab 기능 활용
+
+##### Wordcloud 적용
+vue-worldcloud 이용하여 간단하게 만들 수 있음
+
+##### SSAFY-SR 프로젝트 발대식
+이제 더 바빠질 듯
--- front/sub2/src/views/Home.vue
+++ front/sub2/src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
   <v-app id="inspire">
-
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar
       app
       color="indigo"
       dark
     >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>논문 분석 시스템</v-toolbar-title>
     </v-app-bar>
 
@@ -21,13 +44,70 @@
           <v-col class="text-center">
               <template>
                 <v-file-input
-                    label="File input"
+                    label="논문을 pdf로 업로드해 주세요"
                     filled
                     prepend-icon="mdi-file-pdf"
                     accept=".pdf"
                 ></v-file-input>
               </template>
           </v-col>
+        </v-row>
+        <v-row>
+          <v-row>
+            <v-col>
+              <v-card>
+                <v-tabs
+                  v-model="tab"
+                  fixed-tabs
+                  background-color="indigo"
+                  centered
+                  dark
+                  icons-and-text
+                >
+                  <v-tabs-slider></v-tabs-slider>
+                  <v-tab href="#tab-1">
+                    요약 전체
+                    <v-icon>mdi-animation</v-icon>
+                  </v-tab>
+                  <v-tab href="#tab-2">
+                    연구 주제
+                    <v-icon>mdi-message-alert</v-icon>
+                  </v-tab>
+                  <v-tab href="#tab-3">
+                    연구 방법
+                    <v-icon>mdi-flask-empty</v-icon>
+                  </v-tab>
+                  <v-tab href="#tab-4">
+                    연구 결과
+                    <v-icon>mdi-clipboard</v-icon>
+                  </v-tab>
+                  
+                    <v-tabs-items v-model="tab">
+                      <v-tab-item
+                        v-for="i in 4"
+                        :key="i"
+                        :value="'tab-' + i"
+                      >
+                        <v-card flat>
+                          <v-card-text>Hello World {{ i }}</v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                    </v-tabs-items>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <wordcloud
+                :data="defaultWords"
+                nameKey="keyword"
+                valueKey="frequency"
+                :color="Accent"
+                :wordClick="wordClickHandler">
+              </wordcloud>
+            </v-col>
+          </v-row>
         </v-row>
       </v-container>
     </v-main>
@@ -41,11 +121,58 @@
 </template>
 
 <script>
+  import wordcloud from 'vue-wordcloud'
   export default {
     props: {
       source: String,
     },
+    components: {
+      wordcloud
+    },
+    methods: {
+      wordClickHandler(keyword, frequency, vm) {
+        console.log('wordClickHandler', keyword, frequency, vm)
+      }
+    },
     data: () => ({
+      drawer: null,
+      defaultWords: [{
+          "keyword": "Lux",
+          "frequency": 26
+        },
+        {
+          "keyword": "Syndra",
+          "frequency": 17
+        },
+        {
+          "keyword": "Orianna",
+          "frequency": 14
+        },
+        {
+          "keyword": "Lulu",
+          "frequency": 8
+        },
+        {
+          "keyword": "Viktor",
+          "frequency": 7
+        },
+        {
+          "keyword": "Ezreal",
+          "frequency": 7
+        },
+        {
+          "keyword": "Poppi",
+          "frequency": 4
+        },
+        {
+          "keyword": "Ryze",
+          "frequency": 4
+        },
+        {
+          "keyword": "Jayce",
+          "frequency": 3
+        },
+      ]
     }),
   }
 </script>
