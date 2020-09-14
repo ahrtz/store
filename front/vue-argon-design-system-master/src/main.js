b30c5f37b6dcda916e4bd6e0146eb7ec5a918029
DESKTOP-KVCQHCD\multicampus <myway1513@naver.com>
2020-09-14 17:00:50 +0900

    [Modify]modify main.js

--- front/vue-argon-design-system-master/src/main.js
+++ front/vue-argon-design-system-master/src/main.js
@@ -20,10 +20,12 @@ import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
 import './registerServiceWorker'
+import store from '@/store/index.js'
 
 Vue.config.productionTip = false;
 Vue.use(Argon);
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
