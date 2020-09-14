a6a25f49a563d3d72aa01572a9d0106c88889e54
DESKTOP-KVCQHCD\multicampus <myway1513@naver.com>
2020-09-14 11:31:10 +0900

    [Add]뷰부트스트랩 적용 및 기초프레임 수정, S03P22A406S03P22A406-27

---
+++ front/vue-argon-design-system-master/src/views/Components.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <hero></hero>
+        여기인가요
+        <basic-elements></basic-elements>
+        여기인가요
+        <inputs></inputs>
+        여기인가요
+        <custom-controls></custom-controls>
+        여기인가요
+        <navigation></navigation>
+        여기인가요
+        <javascript-components></javascript-components>
+        여기인가요
+        <icons></icons>
+        여기인가요
+        <examples></examples>
+        여기인가요
+        <download-section></download-section>
+        여기인가요
+        <carousel></carousel>
+    </div>
+</template>
+<script>
+import Hero from "./components/Hero";
+import BasicElements from "./components/BasicElements";
+import Inputs from "./components/Inputs";
+import CustomControls from "./components/CustomControls";
+import Navigation from "./components/Navigation";
+import JavascriptComponents from "./components/JavascriptComponents";
+import Carousel from "./components/Carousel";
+import Icons from "./components/Icons";
+import Examples from "./components/Examples";
+import DownloadSection from "./components/DownloadSection";
+
+export default {
+  name: "components",
+  components: {
+    Hero,
+    BasicElements,
+    Inputs,
+    CustomControls,
+    Navigation,
+    JavascriptComponents,
+    Carousel,
+    Icons,
+    Examples,
+    DownloadSection
+  }
+};
+</script>
