0a551b6a35028ad66b39d4710ecf4bcb6f507282
Inha Yoon <dlsgk147@naver.com>
2020-09-14 12:42:10 +0900

    [MOD] File Directory [ADD] Basic frontend pages

--- front/sub2/src/views/SignUp.vue
+++ front/sub2/src/views/SignUp.vue
@@ -21,7 +21,7 @@
                 flat
               >
                 <v-spacer></v-spacer>
-                <v-toolbar-title>로그인</v-toolbar-title>
+                <v-toolbar-title>회원가입</v-toolbar-title>
                 <v-spacer></v-spacer>
               </v-toolbar>
               <v-card-text>
@@ -40,12 +40,19 @@
                     prepend-icon="mdi-lock"
                     type="password"
                   ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="패스워드 확인"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">회원가입</v-btn>
-                <v-btn color="primary">로그인</v-btn>
+                <v-btn color="primary">가입</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
