e2723916ceb1b442979ed922be998466324261de
Sooni <ksb940925@gmail.com>
2020-09-14 14:15:29 +0900

    [Add][S03P22A406-26] Recommandation algorithm skeleton

--- crawling/analyze_kci.py
+++ crawling/analyze_kci.py
@@ -1,11 +1,10 @@
-from parse_kci import load_dataframes
-import itertools
-from collections import Counter
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import folium as fl
+
+from parse_kci import load_dataframes
+from konlpy.tag import Hannanum
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def parse_keyword(dataframes):
@@ -25,10 +24,66 @@ def parse_keyword(dataframes):
     df = pd.DataFrame()
 
 
+def hannanum_similarity(doc1, doc2):
+    hannanum = Hannanum()
+    str1 = "로봇 및 공작기계용 롤러기어 캠 감속기의 성능 비교 평가"
+    str2 = "착용용 보행보조로봇의 일체형 족관절 토크센서 및 기구 설계"
+    # doc1 = hannanum.nouns(str1)
+    # doc2 = hannanum.nouns(str2)
+    # print(set(doc1) & set(doc2))
+    kkma = Kkma()
+    pprint(kkma.sentences(str2))
+
+
+def jaccard_similarity(doc1, doc2):
+    doc1 = set(doc1)
+    doc2 = set(doc2)
+    return len(doc1 & doc2) / len(doc1 | doc2)
+
+
+def tf_idf_similarity(doc1, doc2):
+    tfidf_vectorizer = TfidfVectorizer(min_df=1)
+    tfidf_matrix = tfidf_vectorizer.fit_transform([doc1, doc2])
+
+    document_distance = (tfidf_matrix * tfidf_matrix.T)
+    # print(document_distance)
+    return document_distance.toarray()[0][1]
+
+
+def get_similarity(dataframes):
+    papers = dataframes["papers"]
+    data = papers.iloc[251, :]  # "성능 인자를 활용한 7자유도 상지 외골격 로봇의 설계"
+    # print(data)
+
+    similarity_list = []
+    for i in range(0, 900):
+        similarity = []
+        similarity.append(tf_idf_similarity(papers["title_en"][i], data.title_en))
+        similarity.append(jaccard_similarity(papers["title_ko"][i], data.title_ko))
+        similarity.append(jaccard_similarity(papers["author"][i], data.author))
+        similarity.append(jaccard_similarity(papers["abstract"][i], data.abstract))
+        similarity.append(jaccard_similarity(papers["subject"][i], data.subject))
+        score = (0.3*similarity[0] + 0.3*similarity[1] + 0.1*similarity[2] + 0.2*similarity[3] + 0.1*similarity[4])
+        similarity_list.append((i, score, similarity))
+        # print(similarity)
+
+    # print(similarity_list)
+    similarity_list = sorted(similarity_list, key=lambda sim: sim[1], reverse=True)
+    # # print(similarity)
+
+    similarity_paper = []
+    for i in range(0, 10):
+        print(similarity_list[i])
+        similarity_paper.append(papers.loc[similarity_list[i][0], :])
+    #
+    print(similarity_paper)
+
+
 def main():
     data = load_dataframes()
-    parse_keyword(data)
-
+    # parse_keyword(data)
+    get_similarity(data)
+    # hannanum_similarity("", "")
 
 if __name__ == "__main__":
     main()
--- crawling/parse_kci.py
+++ crawling/parse_kci.py
@@ -2,6 +2,9 @@ import os
 
 import pandas as pd
 import xlrd
+import requests
+import json
+from googletrans import Translator
 
 DATA_DIR = "."
 DATA_FILE = os.path.join(DATA_DIR, "논문검색리스트 정제 (1).xls")
@@ -39,25 +42,31 @@ def import_data():
     논문 데이터 파일을 읽어서 Pandas DataFrame 형태로 저장합니다
     """
     try:
-        for i in range(1, 2):
-            rb = xlrd.open_workbook(DATA_FILE)
+        cnt = 0
+        papers = []
+        for i in range(1, 4):
+            rb = xlrd.open_workbook("논문검색리스트 정제 ("+str(i)+").xls")
             rb_sheet = rb.sheet_by_index(0)
+
+            nrows = rb_sheet.nrows
+            ncols = rb_sheet.ncols
+
+            for row in range(1, nrows):
+                columns = []
+                for col in range(0, ncols):
+                    if col == 1 or col == 10 or col == 15 or col == 21:
+                        continue
+                    if col == 0:
+                        columns.append(cnt)
+                        cnt += 1
+                        continue
+                    columns.append(rb_sheet.cell_value(row, col))
+
+                papers.append(columns)
+
     except FileNotFoundError as e:
         print(f"`{data_path}` 가 존재하지 않습니다.")
         exit(1)
-
-    nrows = rb_sheet.nrows
-    ncols = rb_sheet.ncols
-
-    papers = []
-    for row in range(1, nrows):
-        columns = []
-        for col in range(0, ncols):
-            if col == 1 or col == 10 or col == 15 or col == 21:
-                continue
-            columns.append(rb_sheet.cell_value(row, col))
-
-        papers.append(columns)
 
     # print(papers)
     paper_frame = pd.DataFrame(data=papers, columns=paper_columns)
@@ -83,12 +92,14 @@ def parse_keyword(dataframes):
         for word in keyword:
             keywords_list.append(word.strip())
 
-    print(keywords_list)
+    print(len(keywords_list))
+    # print(keywords_list)
     keywords_set = set(keywords_list)
-    print(keywords_set)
+    # print(keywords_set)
+    print(len(keywords_set))
 
     keyword_frame = pd.DataFrame(data=keywords_set, columns=keyword_columns)
-    
+
     return {"papers": dataframes["papers"], "keywords": keyword_frame}
 
 
@@ -100,27 +111,84 @@ def load_dataframes():
     return pd.read_pickle(DUMP_FILE)
 
 
+def translation_data(dataframes):
+    """
+    영문을 Google Translation API 를 이용해 한글로 번역
+    :param dataframes: 
+    :return: 
+    """
+    papers = dataframes["papers"]
+    keywords = dataframes["keywords"]
+
+    title_ko = papers["title_ko"]
+    title_en = papers["title_en"]
+    translator = Translator()
+
+    for i in range(0, 900):   # len(title_ko)
+        # 어떤 언어인지 확인
+        str_title_ko = title_ko[i]
+        str_title_en = title_en[i]
+        title_ko_lang = ""
+        title_en_lang = ""
+        if str_title_ko != "":  title_ko_lang = translator.detect(str_title_ko).lang
+        if str_title_en != "":  title_en_lang = translator.detect(str_title_en).lang
+
+        if title_ko_lang == 'ko' and title_en_lang == 'en':     # 두개 다 있는 경우
+            continue
+        elif title_ko_lang == 'en' and title_en_lang == 'ko':   # 두개 위치가 바뀐 경우
+            papers.loc[i, 'title_ko'] = str_title_en
+            papers.loc[i, 'title_en'] = str_title_ko
+        elif title_ko_lang == 'en':                             # 영어인 경우
+            title_trans = translator.translate(str_title_ko, dest="ko")
+            papers.loc[i, 'title_ko'] = title_trans.text
+            papers.loc[i, 'title_en'] = str_title_ko
+        else:                                                   # 한글인 경우
+            title_trans = translator.translate(str_title_ko, dest="en")
+            papers.loc[i, 'title_en'] = title_trans.text
+
+    # print(papers["title_ko"])
+    # print(papers["title_en"])
+
+    return {"papers": dataframes["papers"], "keywords": keywords}
+
+
 if __name__ == '__main__':
     # data = xls_to_json()
 
     print("[*] Parsing data...")
-    data = import_data()
+    # data = import_data()
     print("[+] Done")
 
     print("[*] Parsing keyword...")
-    data = parse_keyword(data)
+    # data = parse_keyword(data)
     print("[+] Done")
 
+    data = load_dataframes()
+    # data_trans = translation_data(data)
+
     print("[*] Dumping data...")
-    dump_dataframes(data)
+    # dump_dataframes(data_trans)
     print("[+] Done\n")
 
-    data = load_dataframes()
-
     print("[논문]")
-    print(data["papers"])
+    print(data["papers"].loc[:, ("title_ko", "title_en")])
     print("\n")
 
     print("[키워드]")
-    print(data["keywords"])
+    # print(data["keywords"])
     print("\n")
+
+    # # 파파고 번역
+    # request_url = "https://openapi.naver.com/v1/papago/n2mt"
+    # text = "Python is an easy to learn, powerful programming language. "
+    #
+    # # !! warning !! 꼭 지우기 !!
+    # headers = {"X-Naver-Client-Id": "", "X-Naver-Client-Secret": ""}
+    # params = {"source": "en", "target": "ko", "text": text}
+    # response = requests.post(request_url, headers=headers, data=params)
+    #
+    # result_json = response.json()
+    # print(result_json)
+    # result = json.loads(result_json)
+    #
+    # print(result["translatedText"])
