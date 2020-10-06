0e23de56c08f6ca61458cb105a815bde236d75a1
Sooni <ksb940925@gmail.com>
2020-10-06 21:25:23 +0900

    [Add][S03P23A406-21] recommend
    Former-commit-id: 386b258e9d779715536eca38c61e4576b82fba7a

--- backend/user_based.py
+++ backend/user_based.py
@@ -2,7 +2,7 @@ from sklearn.decomposition import TruncatedSVD
 from scipy.sparse.linalg import svds
 
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns 
 import pandas as pd
 import numpy as np
 import warnings
