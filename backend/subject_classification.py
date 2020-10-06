b41362f611ce017cdcc25efe3c8eaff9bb11331f
Sooni <ksb940925@gmail.com>
2020-10-06 21:24:05 +0900

    [Add][S03P23A406-21] Add recommend system using keyword
    Former-commit-id: b8368fc49f5fbaea3cf764ae716ec2f728025185

--- backend/subject_classification.py
+++ backend/subject_classification.py
@@ -117,7 +117,7 @@ def get_similarity(tokenized_doc, papers):
     #
     print(df)
 
-def recommend(tokenized_doc, classes):
+def recommend(tokenized_doc, classes, keywords):
     # results = []
     # if len(classes) > 1:
     #     results.append(classes[0][0])
@@ -133,37 +133,30 @@ def recommend(tokenized_doc, classes):
     # classes에서 2개를 가지고
     # sqlite에서 scrap이나 quote가 높은 논문을 500개정도 뽑은 다음에
     # cosine 유사도를 통해 추천을 뽑아냄
-    # papers = pd.read_pickle("./all_data.pkl")
-    # is_subject = papers['subject'] == subjects[results[0]-1]
-    # papers = papers[is_subject]
-    papers = pd.read_pickle("./clean_data_en.pkl")
+    papers = pd.read_pickle("./keyword_space.pkl")
     # is_subject = papers['label'] == results[0]
-    is_subject = papers['subject'] == '화학공학'
+    is_subject = papers['subject'] == '생활과학'
     papers = papers[is_subject]
-    print(papers.columns.tolist())
-    print(papers['keyword_clean'])  #keyword clean 중 split(' ')으로 찢기
     
     papers_df = papers
     papers_df['keyword_clean'] = papers['keyword_clean'].str.split(' ')
     papers_df = papers.explode('keyword_clean')
-    print(papers_df)
+    # print(papers_df)
+    papers_df.dropna(subset=['keyword_clean'])
 
-    contains = df1[df1['key'].str.contains('corrosion')]
-
-    # tokenized_keyword = []
-    # for item in papers['keyword_kor']:
-    #     tokenized_keyword.append(data_preprocessing(item))
-    # # print(tokenized_keyword)
-    # papers['tokenized_keyword'] = tokenized_keyword
-    # df = papers[['id', 'tokenized_keyword']]
-
-    # get_similarity(tokenized_doc, papers)
+    contains = pd.DataFrame()
+    for key in keywords:
+        contains = pd.concat([contains, papers_df[papers_df['keyword_clean'].str.contains(key)]])
+    contains = contains.drop_duplicates(['id'])
+    # print(contains.head(10))
+    return contains.head(5)
 
 
 if __name__ == '__main__':
     abstract = "The effects of flow field upon the distribution of ionic concentration, electric potential, concentration boundary layer thickness, and electric current density were investigated. A modified numerical scheme is proposed to simulate the corresponding electrochemical system which is governed by nonlinear partial differential equations. Seven types of geometries and various flow fields with Reynolds numbers up to 2100 are considered. The obtained results indicate the current numerical method can successfully simulate the increase of current density on the cathode as the applied potential cell increases, and that rise will continue until the limiting current density is reached. To predict the effect of fluid flow, the proposed scheme is applied for various Peclet numbers. The increase of current density for Peclet numbers between 1 and 104 is quite evident. But for large Peclet numbers between 104 and 107 , the current density increases gradually. The results also show that as the anode size is doubled, the maximum current density occurs at the leading and trailing edges. However, if the cathode size is doubled, the maximum current density occurs at the center regions of it. Knowing the regions where current density is extremum helps electochemical system designers to control the parameters of the corresponding process."
-    # abstract = data_preprocessing(abstract)
-    # classes = data_classification(abstract)
-    classes = []
-    recommend(abstract, classes)
+    # abstract = data_preprocessing(abstract) # 요약된 논문의 abstract 데이터 전처리
+    # classes = data_classification(abstract) # 요약된 논문의 분류 구하기
+    classes = []    # 요약된 논문의 분류
+    keywords = ['Textile', 'Handloom', 'structure']   # 요약된 논문의 keyword
+    recommend(abstract, classes, keywords)
 
